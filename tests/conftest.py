ff6e894ea5dc551bb7d63db0df42ea7451d0cffa
George-GS <132917210+George-GS@users.noreply.github.com>
2026-04-15 22:54:46 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,9 +15,6 @@ def driver():
     options = Options()
     options.add_argument('start-maximized')
     options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu')
     driver: WebDriver = webdriver.Chrome(options=options)
     yield driver
 
