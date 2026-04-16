f48347ffab1f2b1c00f03f9bf563eb34aab592b5
George-GS <132917210+George-GS@users.noreply.github.com>
2026-04-16 19:02:55 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,6 +17,9 @@ def driver():
     options = Options()
     options.add_argument('window-size=1920,1080')
     options.add_argument('--headless')
+    options.add_argument('--no-sandbox')  
+    options.add_argument('--disable-dev-shm-usage')  
+    options.add_argument('--disable-gpu')
     driver: WebDriver = webdriver.Chrome(options=options)
     yield driver
     # driver.save_screenshot(f'{random.randint(100, 100000)}.png')
